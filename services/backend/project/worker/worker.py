47e07c48e6b108db6edefe2b9c556a06ff039e03
Jan Boduch <janekb1996@gmail.com>
2019-05-05 16:10:14 +0200

    [NF] Save result in database (mongoDB)

--- services/backend/project/worker/worker.py
+++ services/backend/project/worker/worker.py
@@ -1,13 +1,17 @@
 from celery import Celery
 
 from project.algorithms.crawl import Crawler
-
+from project import crawlerdb
 
 celery = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
+
+mycol = crawlerdb["results"]
 
 
 @celery.task(bind=True)
 def execute_task(self, argument):
     self.update_state(state='STARTED', meta={'argument': argument})
     result = Crawler(item=argument).run()
+    save_element = {"result": result}
+    mycol.insert_one(save_element)
     return {'status': 'Task completed!', 'result': result}
